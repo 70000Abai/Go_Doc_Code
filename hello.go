e2d70b027ca64718716b9e4b04f86fc51c4ae41a
70000Abai <132120480+70000Abai@users.noreply.github.com>
2023-05-11 17:50:15 +0500

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+    "log"
+
+    "example.com/greetings"
+)
+
+func main () {
+	// Set properties of the predefined Logger,including 
+    // the log entry prefix and a flag to disable printing
+	// the time,source files,and line number.
+
+	log.SetPrefix("greetings:")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+
+	message,err := greetings.Hello("Abai")
+	// if an error was returned,print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was returned,print the returned message
+	// to the console.
+
+	fmt.Println(message)
+}
